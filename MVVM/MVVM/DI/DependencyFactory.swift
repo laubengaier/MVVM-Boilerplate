9f4f33c7afd915594b4afe9bcbd95188f9b39239
Mario Zimmermann <m.zimmermann@geomobile.de>
2020-10-13 13:39:31 +0200

    Create Dependency Factory

---
+++ MVVM/MVVM/DI/DependencyFactory.swift
@@ -0,0 +1,8 @@
+//
+//  DependencyFactory.swift
+//  MVVM
+//
+//  Created by Mario Zimmermann on 13.10.20.
+//
+
+import Foundation
---
+++ MVVM/MVVM/DI/GlobalAppDependencies.swift
@@ -0,0 +1,51 @@
+//
+//  GlobalAppDependencies.swift
+//  MVVM
+//
+//  Created by Mario Zimmermann on 13.10.20.
+//
+
+import Foundation
+
+public protocol GlobalAppDependencies: HasAPIClient &
+                                       HasAPIKey {}
+
+public struct AppDependencies: GlobalAppDependencies {
+    public var apiKey: String
+    public var apiClient: APIClient
+}
+
+public protocol DependencyFactoryProtocol {
+    /**
+     Creates each and every dependency we need in our project
+    - Parameter window:Backdrop for the app's UI
+    - Returns: AppDependencies
+     */
+    func create() -> AppDependencies
+}
+
+public class DependencyFactory: DependencyFactoryProtocol {
+    
+    public init(){}
+    
+    public func create() -> AppDependencies {
+        
+        let apiKey: String = self.makeAPIKey()
+        
+        let apiClient: APIClient = self.makeAPIClient(apiKey: apiKey)
+        
+        return AppDependencies(apiKey: apiKey,
+                               apiClient: apiClient)
+        
+    }
+    
+    private func makeAPIKey() -> String {
+        let ret: String = "4c0b8d236e387a7c24bc97ca0b0ecf03"
+        return ret
+    }
+    
+    private func makeAPIClient(apiKey: String) -> APIClient {
+        let ret: APIClient = APIClient(apiKey: apiKey)
+        return ret
+    }
+}
