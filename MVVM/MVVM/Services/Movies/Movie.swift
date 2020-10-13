67c6ca0c512c2162cea42067b23d505999261f8c
Timotheus (MacMini) <timotheus@laubengaier.com>
2020-10-13 19:47:50 +0900

    fix

--- MVVM/MVVM/Services/Movies/Movie.swift
+++ MVVM/MVVM/Services/Movies/Movie.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct Movie : Codable {
+    
+    let id: Int
+    let title: String
+    let posterPath: String
+    let backdropPath: String
+    let voteAverage: Float
+    let overview: String
+    let releaseDate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+        case voteAverage = "vote_average"
+        case overview
+        case releaseDate = "release_date"
+    }
+    
+}
