d258a220eebcc40cb0b9650164e109293ec0a0c8
Mario Zimmermann <m.zimmermann@geomobile.de>
2020-10-13 13:42:16 +0200

    Refac

--- MVVM/MVVM/DI/DependencyFactory.swift
+++ MVVM/MVVM/DI/DependencyFactory.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+public class DependencyFactory: DependencyFactoryProtocol {
+    
+    public init(){}
+    
+    public func create() -> AppDependencies {
+        
+        let apiKey: String = self.makeAPIKey()
+        
+        let apiClient: APIClient = self.makeAPIClient(apiKey: apiKey)
+        
+        return AppDependencies(apiKey: apiKey,
+                               apiClient: apiClient)
+        
+    }
+    
+    private func makeAPIKey() -> String {
+        let ret: String = "4c0b8d236e387a7c24bc97ca0b0ecf03"
+        return ret
+    }
+    
+    private func makeAPIClient(apiKey: String) -> APIClient {
+        let ret: APIClient = APIClient(apiKey: apiKey)
+        return ret
+    }
+}
