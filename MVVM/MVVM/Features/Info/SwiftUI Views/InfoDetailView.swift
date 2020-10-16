6c50bf214380c1d03aab8cb3a3e91084b262cd31
Timotheus (MacMini) <timotheus@laubengaier.com>
2020-10-16 17:45:22 +0900

    make use of passthroughsubject

---
+++ MVVM/MVVM/Features/Info/SwiftUI Views/InfoDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  InfoDetailView.swift
+//  MVVM
+//
+//  Created by Timotheus Laubengaier on 16.10.20.
+//
+
+import SwiftUI
+
+struct InfoDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoDetailView()
+    }
+}
